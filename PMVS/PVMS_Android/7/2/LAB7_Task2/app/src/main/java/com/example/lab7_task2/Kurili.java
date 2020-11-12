4150faad52e3dc5b2716d171c9d6a57d7fcb323c
elya701 <karlmarten2001@gmail.com>
2020-11-12 11:32:15 +0300

    feat: add completed task2(MultiScreen) PMVS_LAB7

---
+++ PMVS/PVMS_Android/7/2/LAB7_Task2/app/src/main/java/com/example/lab7_task2/Kurili.java
@@ -0,0 +1,14 @@
+package com.example.lab7_task2;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Kurili extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.kurili);
+    }
+}
